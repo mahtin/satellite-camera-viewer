c38c23ccaf9fd0b9b30118d153fd3db503d918fe
Martin Levy <mahtin@mahtin.com>
2026-07-15 09:49:44 -0700

    vector returned, example code changed

--- src/SatelliteCameraViewer/BSC5Stars.py
+++ src/SatelliteCameraViewer/BSC5Stars.py
@@ -106,6 +106,14 @@ class BSC5Stars:
 		return self._stars
 
 	@property
+	def vector(self):
+		"""
+		vector - return an array of stars as a vector
+		"""
+		xyz = self.skycoords.cartesian.xyz.value
+		return xyz.T   # shape (N,3)
+
+	@property
 	def skycoords(self):
 		"""
 		skycoords - return an array of stars (in SkyCoord format).
@@ -172,20 +180,20 @@ class BSC5Stars:
 
 def _main(args=None):
 	""" _main """
-	b = BSC5Stars()
-	s = b.stars
-	print(s[0:10])
+	b = BSC5Stars(max_mag=3)
+	for s in [b.stars, b.skycoords, b.vector]:
+		print('[')
+		for ii in range(5):
+			print('\t'+str(s[ii]).replace('\n', ' ')+',')
+		print(']')
+		print('')
+
 	#[
 	#	Star(number=2491, name='9Alp',  constellation='CMa', ra=1.7677930939085398, dec=-0.29175117701809655, mag=-1.46),
 	#	Star(number=7001, name='3Alp',  constellation='Lyr', ra=4.87356286460115,   dec=0.6769017097019452,   mag=0.03),
 	#	Star(number=1708, name='13Alp', constellation='Aur', ra=1.3818208020352105, dec=0.802817518959714,    mag=0.08),
 	#	Star(number=1713, name='19Bet', constellation='Ori', ra=1.3724323851005242, dec=-0.14314608748440158, mag=0.12),
 	#	Star(number=2943, name='10Alp', constellation='CMi', ra=2.0040815858077057, dec=0.09119345341670373,  mag=0.38),
-	#	Star(number=472,  name='Alp',   constellation='Eri', ra=0.4263621196465648, dec=-0.998968286199821,   mag=0.46),
-	#	Star(number=2061, name='58Alp', constellation='Ori', ra=1.5497287482822817, dec=0.12927556806785778,  mag=0.5 ),
-	#	Star(number=5267, name='Bet',   constellation='Cen', ra=3.6818738679550713, dec=-1.0537085989338988,  mag=0.61),
-	#	Star(number=7557, name='53Alp', constellation='Aql', ra=5.195772461134952,  dec=0.15478161583103048,  mag=0.77),
-	#	Star(number=1457, name='87Alp', constellation='Tau', ra=1.2039281180256884, dec=0.2881393150938305,   mag=0.85)
 	#]
 
 if __name__ == '__main__':
