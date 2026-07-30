3bb71944b34dcd21e2465b5e72ff69282d5c71a7
Martin Levy <mahtin@mahtin.com>
2026-07-30 18:15:37 +0100

    added PESSOA to test IDs above 100,000. Plus format cleanup

--- src/SatelliteCameraViewer/static_list_satellites.py
+++ src/SatelliteCameraViewer/static_list_satellites.py
@@ -5,19 +5,20 @@ from dataclasses import dataclass
 # Define which satellite we keep setup for all runs of the program
 # Make ISS the default (i.e. first position) - just because
 
-@dataclass
+@dataclass(frozen=True)
 class StaticListSatellite:
-    """ StaticListSatellite """
-    sat_id: int = None
-    """ sat_id - Satellite ID """
-    name: str = ''
-    """ name - name of the satellite """
+	""" StaticListSatellite """
+	sat_id: int = None
+	""" sat_id - Satellite ID """
+	name: str = ''
+	""" name - name of the satellite """
 
 static_list_satellites = [
-        StaticListSatellite(25544, 'ISS (ZARYA)'),
-        StaticListSatellite(36411, 'EWS-G2 (GOES 15)'),
-        StaticListSatellite(58584, 'ARKTIKA-M 2'),
-        StaticListSatellite(49260, 'LANDSAT 9'),
-        StaticListSatellite(48274, 'CSS (TIANHE)'),
-        StaticListSatellite(20580, 'HST'),
+	StaticListSatellite(25544,	'ISS (ZARYA)'),
+	StaticListSatellite(36411,	'EWS-G2 (GOES 15)'),
+	StaticListSatellite(58584,	'ARKTIKA-M 2'),
+	StaticListSatellite(49260,	'LANDSAT 9'),
+	StaticListSatellite(48274,	'CSS (TIANHE)'),
+	StaticListSatellite(20580,	'HST'),
+	StaticListSatellite(100139,	'PESSOA'),
 ]
